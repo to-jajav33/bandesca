5f316f425d238bc5934026a8296f5e73c810bb09
Javier Mendoza <to.jajav33@gmail.com>
2026-04-04 16:07:01 -0700

    adding musicTrackSelector

---
+++ src/pages/public/components/musicTrackSelector/musicTrackSelector.client.js
@@ -0,0 +1,17 @@
+import { CustomComponent } from "../customComponent.client";
+import * as template from "./musicTrackSelector.dep.html" with { type: "text" };
+import * as musicTrackSelectorCss from "./musicTrackSelector.dep.module.css" with { type: "css" };
+
+export class MusicTrackSelector extends CustomComponent {
+  data = {
+    css: musicTrackSelectorCss,
+    types: ["midi", "audio", "voice", "none"],
+    selectedType: "midi",
+  };
+
+  constructor() {
+    super();
+  }
+}
+
+MusicTrackSelector.define(template.default);
