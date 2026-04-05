b59712ee35fb5581f7c203ff45a5ef99003a142d
Javier Mendoza <to.jajav33@gmail.com>
2026-04-05 12:58:41 -0700

    adding inner shadoow to infinite scroll when available

--- src/pages/public/components/musicTrackInfiniteScroll/musicTrackInfiniteScroll.client.js
+++ src/pages/public/components/musicTrackInfiniteScroll/musicTrackInfiniteScroll.client.js
@@ -6,6 +6,60 @@ export class MusicTrackInfiniteScroll extends CustomComponent {
   data = {
     css: musicTrackInfiniteScrollCss,
   };
+
+  constructor() {
+    super();
+
+    this.handleScroll = this.handleScroll.bind(this);
+    const contentContainer = this.shadowRoot.querySelector(
+      "[ref='contentContainer']",
+    );
+    contentContainer.addEventListener("scroll", this.handleScroll);
+  }
+
+  connectedCallback() {
+    requestAnimationFrame(() => {
+      this.handleScroll();
+    });
+  }
+
+  handleScroll() {
+    // add an inset shadow on right if scrollable to the right to the right of the content
+    const contentContainer = this.shadowRoot.querySelector(
+      "[ref='contentContainer']",
+    );
+
+    const insetBlur = 10;
+    let left = 0;
+    let right = 0;
+    let top = 0;
+    let bottom = 0;
+    let offsetFactor = 0.75;
+
+    if (contentContainer.scrollTop > 0) {
+      top = insetBlur * offsetFactor;
+    }
+    if (
+      contentContainer.scrollHeight - contentContainer.scrollTop >
+      contentContainer.clientHeight
+    ) {
+      bottom = -insetBlur * offsetFactor;
+    }
+    if (contentContainer.scrollLeft > 0) {
+      left = insetBlur * offsetFactor;
+    }
+    if (
+      contentContainer.scrollWidth - contentContainer.scrollLeft >
+      contentContainer.clientWidth
+    ) {
+      right = -insetBlur * offsetFactor;
+    }
+
+    contentContainer.style.boxShadow = `inset ${left}px 0 ${insetBlur}px -${insetBlur}px var(--primary-color),
+    inset ${right}px 0 ${insetBlur}px -${insetBlur}px var(--primary-color),
+    inset ${top}px 0 ${insetBlur}px -${insetBlur}px var(--primary-color),
+    inset ${bottom}px 0 ${insetBlur}px -${insetBlur}px var(--primary-color)`;
+  }
 }
 
 MusicTrackInfiniteScroll.define(template.default);
